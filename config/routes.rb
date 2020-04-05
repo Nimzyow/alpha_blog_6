77f8bc4b4724253850cd1677db34caa5169be38b
Nimzyow <n_soufiani@hotmail.com>
2020-04-05 18:39:45 +0100

    added comments for futher clarification

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  #rails automatically knows that when we write pages, like we did below, it knows its the pages_controller that we are referring to.
   root "pages#home"
+  # the words after # is simply the action/method on the controller to the left of the #
   get "about", to: "pages#about"
   resources :articles, only: [:show]
 end
