d2165eb09466dfb04f7e4524cee46353f4fe02da
Nimzyow <n_soufiani@hotmail.com>
2020-04-05 23:50:02 +0100

    created a route for articles/new ,updated articles_controller and created new.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,5 +6,17 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  def new
+    
+  end
 
+  def create
+    #below just prints our the params straight to the page
+    #render plain: params[:article]
+    #we cant send the article directly into Article.new. There is an issue with security and so we need to send it as we have done below.
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    #render plain: @article.inspect
+    @article.save
+    redirect_to article_path(@article)
+  end
 end
