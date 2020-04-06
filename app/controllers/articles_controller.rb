6d0794a64b4cd413c1384e2f2f44bdf5715dcb57
Nimzyow <n_soufiani@hotmail.com>
2020-04-06 10:54:30 +0100

    refactored articles index, edit and new erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,27 +1,26 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:show, :edit, :update, :destroy ]
+
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
     @articles = Article.all
   end
-  
+
   def new
     @article = Article.new
   end
 
   def edit
-    
-    @article = Article.find(params[:id])
   end
 
   def create
     #below just prints our the params straight to the page
     #render plain: params[:article]
     #we cant send the article directly into Article.new. There is an issue with security and so we need to send it as we have done below.
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     #render plain: @article.inspect
     if @article.save
       flash[:notice] = "Article was created successfully"
@@ -32,8 +31,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
     flash[:notice] = "Article was updated successfully"
     redirect_to article_path(@article)
     else
@@ -42,8 +40,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
